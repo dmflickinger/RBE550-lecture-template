6fd4f1cb735d0e7ac570990ccbc228a53c3b640b
Daniel Montrallo Flickinger <daniel.m.flickinger@lmco.com>
2020-08-20 22:12:41 -0400

    adjust title page formatting
    Make date on the title page format nicer, with smaller gray text, and fancy date.
    Also include the course title from a file instead of hard coding it.

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ cp -f fig/*.png /usr/local/share/LaTeX_templates/RBE550_lecture/fig/
 
 cp -f template/RBE-550_S2021_lecture_template_preamble.tex /usr/local/share/LaTeX_templates/RBE550_lecture
 cp -f template/RBE-550_S2021_lecture_template_final_slide.tex /usr/local/share/LaTeX_templates/RBE550_lecture
+cp -f template/course_title.tex /usr/local/share/LaTeX_templates/RBE550_lecture
