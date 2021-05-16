fe8d9ec35ec163365c9064bb1b45a16fdb902637
Daniel Montrallo Flickinger, PhD <daniel.m.flickinger@lmco.com>
2021-05-16 14:16:05 -0400

    update title page style
    Updated the title page style to use the correct colors.
    Also disabled video encoding (for now?)
    Fixed formatting on the TOC pages.

--- scripts/encodeVideo.sh
+++ scripts/encodeVideo.sh
@@ -6,7 +6,7 @@ pdftocairo -png -scale-to-x -1 -scale-to-y 2160 $1.pdf slideImages/slide
 
 
 # Now encode those pngs to video
-ffmpeg -y -r 1/5 -i slideImages/slide-%02d.png -c:v libx264 -r 30 -pix_fmt yuv420p $1.m4v
+# ffmpeg -y -r 1/5 -i slideImages/slide-%02d.png -c:v libx264 -r 30 -pix_fmt yuv420p $1.m4v
 
 
 # Compress the slide images, and remove
