c3f81e6ba7b5ce3ae451c7da0ae90f572d3e2411
Daniel Montrallo Flickinger <daniel.m.flickinger@lmco.com>
2025-09-07 17:51:09 +0000

    implement test utility to generate audio files from slides

--- unitTests/createAudioFiles.py
+++ unitTests/createAudioFiles.py
@@ -1,20 +1,28 @@
 from gtts import gTTS
 import os
 from pydub import AudioSegment
+import argparse
+from PyPDF2 import PdfReader
 
 def save_mp3(text, filename):
     tts = gTTS(text=text, lang='en', slow=False)
     tts.save(f'{filename}.mp3')
     print(f"saved {filename}.mp3")
 
-
 if __name__ == "__main__":
 
-    # TODO: read in command line arguments
-    # TODO: create mp3 files in a specified directory
-    # TODO: create multiple mp3 files (one for each slide)
-    # TODO: count the number of pages to calculate the number of files
-    # TODO: extract and convert the title text to speech
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="PDF file path")
+    parser.add_argument("output", help="output directory for audio files")
+    args = parser.parse_args()
 
-    text = "Hello, this is a text-to-speech MP3 example."
-    save_mp3(text, 'example')
+    with open(args.file, 'rb') as slides_file:
+        reader = PdfReader(slides_file)
+
+        num_slides = len(reader.pages)
+        print(f"{num_slides} slides in {args.file}")
+
+        page_idx = 1
+        for page in reader.pages:
+            save_mp3(page.extract_text(), f"{args.output}/slide_{page_idx}.mp3")
+            page_idx += 1
